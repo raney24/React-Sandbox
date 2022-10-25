5f8f4b60c647ef3584883310ff7265d5017300aa
Kyle Raney <raney24fcb@gmail.com>
2022-10-25 10:01:26 -0400

    React 1.17 bst

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  SkillTreeGroup,
+  SkillTree,
+  SkillProvider,
+  SkillType,
+  SkillGroupDataType,
+  SavedDataType,
+} from 'beautiful-skill-tree';
+
+import { basics } from './hardcodedTree';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <SkillProvider>
+        <SkillTreeGroup>
+          {(skillCount) => {
+            return (
+              <>
+                <SkillTree
+                  treeId="construct3basics"
+                  title="Skill Tree"
+                  description="First skill tree"
+                  data={basics}
+                />
+              </>
+            )
+          }}
+        </SkillTreeGroup>
+      </SkillProvider>
       </header>
     </div>
   );
